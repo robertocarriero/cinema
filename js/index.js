d06d3a60d8cbfb02bcb8f8e10588952a2e25202c
Roberto Carriero <roberto_carriero1@yahoo.com.arm>
2023-08-11 03:06:50 -0300

    First Version

---
+++ js/index.js
@@ -0,0 +1,67 @@
+const URL_PATH = 'https://api.themoviedb.org';
+const API_KEY = '0100095a9f96d25d2f3a2f69ca848e5b';
+
+document.addEventListener('DOMContentLoaded',  () => {
+    renderNewsMovies();
+    renderMoviesHome('popular','now-playing');
+    renderMoviesHome('top_rated','top-rated-playing');
+})
+
+const getMovies = (url) => {
+    const URL = `${URL_PATH}/3/movie/${url}?api_key=${API_KEY}&language=es-ES&page=1`
+    return fetch(URL)
+        .then(resp => resp.json())
+        .then(result => result.results)
+        .catch(e => console.log(e));
+}
+
+const renderNewsMovies = async () => {
+    const movies = await getMovies('now_playing');
+    let html = '';
+    movies.forEach((movie, index) => {
+        const {id, title, overview, backdrop_path} = movie;
+        const titleImage = 'https://image.tmdb.org/t/p/original' + backdrop_path;
+        const urlMovie = `movie.html?id=${id}`
+        html += `
+            <div class="carousel-item ${index === 0 ? 'active': null}" style="background-image: url('${titleImage}')">
+                <div class="carousel-caption">
+                    <h5>${title}</h5>
+                    <p class="">${overview}</p>
+                    <a href="${urlMovie}" class="btn btn-primary">Más información</a>
+                </div>
+            </div>
+        `
+    });
+    html += `
+        <a class="carousel-control-prev" href="#carousel-news-movies" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="carousel-control-next" href="#carousel-news-movies" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    `
+    document.getElementsByClassName('list-new-movies')[0].innerHTML = html;
+}
+
+const renderMoviesHome = async (url, clase) =>{
+    const movies = await getMovies(url);
+    let html = '';
+    movies.forEach((movie,index) => {
+        const {id, title, poster_path } = movie;
+        const titleImage = 'https://image.tmdb.org/t/p/w500' + poster_path;
+        const urlMovie = `movie.html?id=${id}`
+        if(index < 20){
+            html += `
+                <li class="list-group-item">
+                    <div class="imagen-home"><img src="${titleImage}" alt="${title}"></div>
+                    <h3>${title}</h3>
+                    <a href="${urlMovie}" class="btn btn-outline-primary">Ver Más</a>
+                </li>
+            `
+        }
+    });
+    document.getElementsByClassName(`${clase}__list`)[0].innerHTML = html;
+}
+
